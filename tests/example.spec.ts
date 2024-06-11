336a45d57591a4ad1dfff00ce4b9dd229cb125eb
jan-beranek <86586714+jan-beranek@users.noreply.github.com>
2024-06-11 22:09:11 +0200

    feat: upgrade to 4.0.0

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,31 +4,89 @@ import { test } from '@playwright/test';
 test.describe('test', () => {
   let wopee: Wopee;
   test.beforeAll(async () => {
-    wopee = new Wopee();
-    await wopee.startSuite(`test-${Math.floor(Math.random() * 1000) + 1}`);
+    wopee = new Wopee();    
+    
+    const now = new Date();
+    const date = `${now.getDate().toString().padStart(2, '0')}-${(now.getMonth() + 1).toString().padStart(2, '0')}-${now.getFullYear()}`;
+    await wopee.startSuite(`test-${date}`);
+  });
+
+  test.afterEach(async () => {
+    await wopee.stopScenario();
   });
 
   test('trackImage', async ({ page }) => {
-    await wopee.startScenario('trackImage');
-
     await page.goto('https://playwright.dev/');
 
     const screenshot = await page.screenshot({ type: 'png' });
     const imageBase64 = screenshot.toString('base64');
-    const randomNumber = Math.floor(Math.random() * 1000) + 1;
-    await wopee.trackImage(`image-${randomNumber}`, imageBase64);
-
-    await wopee.stopScenario();
+    
+    await wopee.trackImage({
+      browser: 'browser',
+      comment: 'comment',
+      customTags: 'customTags',
+      device: 'device',
+      imageBase64: imageBase64,
+      os: 'os',
+      pixelToPixelDiffTolerance: 1.0,
+      scenarioName: 'trackImageWithoutStartScenario',
+      stepName: 'image-trackImage',
+      viewport: 'viewport',
+    });
   });
 
   test('trackFullPageScreenshot', async ({ page }) => {
-    await wopee.startScenario('trackFullPageScreenshot');
-
     await page.goto('https://playwright.dev/');
 
-    const randomNumber = Math.floor(Math.random() * (1000 - 1 + 1)) + 1;
-    await wopee.trackFullPageScreenshot(page, `image-${randomNumber}`);
+    await wopee.trackFullPage({
+      browser: 'browser',
+      comment: 'comment',
+      customTags: 'customTags',
+      device: 'device',
+      os: 'os',
+      page: page,
+      pixelToPixelDiffTolerance: 1.0,
+      scenarioName: 'trackFullPageScreenshotWithoutStartScenario',
+      stepName: 'image-trackFullPageScreenshot',
+      viewport: 'viewport',
+    });
+  });
 
-    await wopee.stopScenario();
+  test('trackElement', async ({ page }) => {
+    await page.goto('https://playwright.dev/');
+
+    const element = page.locator('//a[@class="getStarted_Sjon"]');
+
+    await wopee.trackElement({
+      browser: 'browser',
+      comment: 'comment',
+      customTags: 'customTags',
+      device: 'device',
+      locator: element,
+      os: 'os',
+      pixelToPixelDiffTolerance: 1.0,
+      scenarioName: 'trackElementWithoutStartScenario',
+      stepName: 'element',
+      viewport: 'viewport',
+    });
+  });
+
+  test('trackViewport', async ({ page }) => {
+    await page.goto('https://playwright.dev/');
+
+    const element = page.locator('//a[@class="getStarted_Sjon"]');
+
+    await wopee.trackElement({
+      browser: 'browser',
+      comment: 'comment',
+      customTags: 'customTags',
+      device: 'device',
+      locator: element,
+      os: 'os',
+      pixelToPixelDiffTolerance: 1.0,
+      scenarioName: 'trackViewportWithoutStartScenario',
+      stepName: 'viewport',
+      viewport: 'viewport',
+    });
   });
 });
