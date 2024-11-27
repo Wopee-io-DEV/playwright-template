c124a17d0be78d7ac478aa958bcf944b74b378d9
Marcel Veselka <marcel-veselka@users.noreply.github.com>
2024-11-27 12:32:52 +0100

    Feat/template project enhancements (#4)
    * chore: small improvements

--- tests/native/1-first.spec.ts
+++ tests/native/1-first.spec.ts
@@ -1,7 +1,6 @@
 import { test, expect } from "@playwright/test";
 
 const baseUrl = "https://dronjo.wopee.io";
-// const baseUrl = "https://dronjo.wopee.io/gallery.html";
 
 test("Dronjo home page - visual test", async ({ page }) => {
   await page.goto(baseUrl);
