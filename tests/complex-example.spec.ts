2c1f13b82e46815e396afe8ad815682a1eddee6b
jan-beranek <86586714+jan-beranek@users.noreply.github.com>
2024-11-30 22:16:26 +0100

    feat: finetune complex example

--- tests/complex-example.spec.ts
+++ tests/complex-example.spec.ts
@@ -9,11 +9,15 @@ test.describe("Complex Example", () => {
     await wopee.startSuite(`Complex Example ${new Date().toISOString().substring(0, 16)}`);
   });
 
+  test.beforeEach(async () => {
+    await wopee.startScenario(test.info().title);
+  });
+
   test.afterEach(async () => {
     await wopee.stopScenario();
   });
 
-  test("trackFullPage", async ({ page }, testInfo) => {
+  test("track*", async ({ page }, testInfo) => {
     await page.goto("https://playwright.dev/");
 
     await wopee.trackFullPage({
@@ -21,28 +25,18 @@ test.describe("Complex Example", () => {
       customTags: "complex",
       page: page,
       pixelToPixelDiffTolerance: 1.0,
-      scenarioName: testInfo.title,
-      stepName: "fullpage",
+      stepName: "trackFullPage",
       viewport: "viewport",
     });
-  });
-
-  test("trackElement", async ({ page }, testInfo) => {
-    await page.goto("https://playwright.dev/");
 
     await wopee.trackElement({
       comment: "See the Repo tab above for more details about implementation.",
       customTags: "complex",
       locator: page.locator('//a[@class="getStarted_Sjon"]'),
       pixelToPixelDiffTolerance: 1.0,
-      scenarioName: testInfo.title,
-      stepName: "element",
+      stepName: "trackElement",
       viewport: "viewport",
     });
-  });
-
-  test("trackImage", async ({ page }, testInfo) => {
-    await page.goto("https://playwright.dev/");
 
     const screenshot = await page.screenshot({ type: "png" });
     await wopee.trackImage({
@@ -50,8 +44,7 @@ test.describe("Complex Example", () => {
       customTags: "complex",
       imageBase64: screenshot.toString("base64"),
       pixelToPixelDiffTolerance: 1.0,
-      scenarioName: testInfo.title,
-      stepName: "image",
+      stepName: "trackImage",
       viewport: "viewport",
     });
   });
