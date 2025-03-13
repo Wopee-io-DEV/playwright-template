51101e11f7bd9248b73bb214082765b38de8cb0e
jan-beranek <86586714+jan-beranek@users.noreply.github.com>
2025-03-13 14:30:16 +0100

    feat: Set suite name if provided

--- tests/simple.spec.ts
+++ tests/simple.spec.ts
@@ -4,7 +4,7 @@ import { test } from "@playwright/test";
 let wopee: Wopee = new Wopee();
 
 test("Home Page", async ({ page }, testInfo) => {
-  await wopee.startSuite(`Simple Example ${new Date().toLocaleString()}`);
+  await wopee.startSuite(process.env.WOPEE_SUITE_NAME || `Simple Example ${new Date().toLocaleString()}`);
   await wopee.startScenario(test.info().title, testInfo);
 
   await page.goto(process.env.WOPEE_PROJECT_URL || "https://dronjo.wopee.io");
